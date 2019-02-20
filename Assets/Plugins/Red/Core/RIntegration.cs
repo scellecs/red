97e45d106ce9081ca94edaf88f9e20df43dec0d8
Oleg Morozov <benjminmoore@gmail.com>
2019-02-20 18:32:45 +0300

    Add bridge for integrate modules between themselves
    Signed-off-by: Oleg Morozov <benjminmoore@gmail.com>

--- Assets/Plugins/Red/Core/RIntegration.cs
+++ Assets/Plugins/Red/Core/RIntegration.cs
@@ -4,7 +4,16 @@ namespace Red {
 
     public interface IIntegrationState {
     }
-    
+
+    public interface IIntegrationBridge {
+        void Inject<T> (T originState) where T : IIntegrationState;
+    }
+
+    public interface IIntegrationState<in T> : IIntegrationState
+        where T : IIntegrationBridge {
+        void Inject(T input);
+    }
+
     public interface IIntegrationRoot<in TC, in TS>
         where TC : IIntegrationConfiguration
         where TS : IIntegrationState {
