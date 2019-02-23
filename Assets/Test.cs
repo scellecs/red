01d71628460a04cdbe9372a19ce5bd0d8fa287b8
Oleg Morozov <benjminmoore@gmail.com>
2019-02-23 16:45:19 +0300

    Move all dependecies for NSubstitute into one folder
    Signed-off-by: Oleg Morozov <benjminmoore@gmail.com>

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Red;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+public class Test : MonoBehaviour {
+    public ReactiveProperty<int> origin = new ReactiveProperty<int>();
+
+    private void Update() {
+        this.origin.Value = Random.Range(0, 100);
+    }
+}
+
+
+//
+//public static class App {
+//    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+//    public static async void Main() {
+//    }
+//}
