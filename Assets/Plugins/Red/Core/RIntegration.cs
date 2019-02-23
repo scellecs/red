59c5cb4de709dfa3d473325812ddead43ac6ea70
Oleg Morozov <benjminmoore@gmail.com>
2019-02-23 16:44:47 +0300

    Add defines for RIntegration file
    Signed-off-by: Oleg Morozov <benjminmoore@gmail.com>

--- Assets/Plugins/Red/Core/RIntegration.cs
+++ Assets/Plugins/Red/Core/RIntegration.cs
@@ -1,3 +1,4 @@
+#if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
 namespace Red {
     public interface IIntegrationConfiguration {
     }
@@ -20,3 +21,4 @@ namespace Red {
         void Integrate(TC configuration, TS state, IObservableScheduler scheduler);
     }
 }
+#endif
