986eb88f35c891bf400af9da24206b90e89e268a
Oleg Morozov <benjminmoore@gmail.com>
2019-02-23 19:02:22 +0300

    Improve RAssemblyDefinition
    Signed-off-by: Oleg Morozov <benjminmoore@gmail.com>

--- Assets/Plugins/Red/Editor/RAssemblyDefinitionDebug.cs
+++ Assets/Plugins/Red/Editor/RAssemblyDefinitionDebug.cs
@@ -37,14 +37,16 @@ namespace Red.Editor {
 
         private static void CompilationPipelineOnAssemblyCompilationFinished(string assembly, CompilerMessage[] arg2) {
             var timeSpan = DateTime.UtcNow - StartTimes[assembly];
+            
             compilationTotalTime += timeSpan.TotalMilliseconds;
-            BuildEvents.AppendFormat("{0} {1:0.00}s\n", assembly.Substring(ScriptAssembliesPathLen, assembly.Length - ScriptAssembliesPathLen),
+            BuildEvents.AppendFormat("  • <i>{0}</i> {1:0.00} seconds\n", assembly.Substring(ScriptAssembliesPathLen, assembly.Length - ScriptAssembliesPathLen),
                 timeSpan.TotalMilliseconds / 1000f);
         }
 
         private static void AssemblyReloadEventsOnBeforeAssemblyReload() {
             var totalCompilationTimeSeconds = compilationTotalTime / 1000f;
-            BuildEvents.AppendFormat("Compilation total: {0:0.00}s\n", totalCompilationTimeSeconds);
+            BuildEvents.AppendFormat("―――――――――――――――――――――――――\n" +
+                                     " <b>Compilation Total:</b> {0:0.00} seconds\n", totalCompilationTimeSeconds);
             EditorPrefs.SetString(ASSEMBLY_RELOAD_EVENTS_EDITOR_PREF, DateTime.UtcNow.ToBinary().ToString());
             EditorPrefs.SetString(ASSEMBLY_COMPILATION_EVENTS_EDITOR_PREF, BuildEvents.ToString());
             EditorPrefs.SetString(ASSEMBLY_TOTAL_COMPILATION_TIME_EDITOR_PREF, totalCompilationTimeSeconds.ToString(CultureInfo.InvariantCulture));
@@ -63,8 +65,10 @@ namespace Red.Editor {
                 var compilationTimes = EditorPrefs.GetString(ASSEMBLY_COMPILATION_EVENTS_EDITOR_PREF);
                 var totalTimeSeconds = totalCompilationTimeSeconds + time.TotalSeconds;
                 if (!string.IsNullOrEmpty(compilationTimes)) {
-                    Debug.Log($"Compilation Report: {totalTimeSeconds:F2} seconds\n" + compilationTimes + "Assembly Reload Time: " + time.TotalSeconds +
-                              "s\n");
+                    Debug.Log($"<b>Compilation Report</b>: {totalTimeSeconds:F2} seconds\n" +
+                              "―――――――――――――――――――――――――\n" +
+                              $"{compilationTimes}" +
+                              $" <b>Assembly Reload Time:</b> {time.TotalSeconds:F2} seconds\n");
                 }
             }
         }
