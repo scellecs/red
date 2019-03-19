37e636ff0b34f4a5f834b721e0e56714f78e4c21
Oleg Morozov <benjminmoore@gmail.com>
2019-03-19 19:17:46 +0300

    Add xcrew.dev registry Add [NotNull] attribute in Integration
    Signed-off-by: Oleg Morozov <benjminmoore@gmail.com>

--- Assets/Plugins/Red/Pure/Integration/IIntegrationRoot.cs
+++ Assets/Plugins/Red/Pure/Integration/IIntegrationRoot.cs
@@ -1,9 +1,11 @@
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
 namespace Red.Pure.Integration {
+    using JetBrains.Annotations;
+
     public interface IIntegrationRoot<in TC, in TS>
         where TC : IIntegrationConfiguration
         where TS : IIntegrationState {
-        void Integrate(TC configuration, TS state, IObservableScheduler scheduler);
+        void Integrate([NotNull] TC configuration, [NotNull] TS state, [NotNull] IObservableScheduler scheduler);
     }
 }
 #endif
--- Assets/Plugins/Red/Pure/Integration/IIntegrationState.cs
+++ Assets/Plugins/Red/Pure/Integration/IIntegrationState.cs
@@ -1,11 +1,16 @@
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
 namespace Red.Pure.Integration {
+    using JetBrains.Annotations;
+
     public interface IIntegrationState {
     }
-    
-    public interface IIntegrationState<in T> : IIntegrationState
+
+    public interface IIntegrationState<T> : IIntegrationState
         where T : IIntegrationBridge {
-        void Inject(T bridge);
+#if ZENJECT
+        [Inject]
+#endif
+        T Bridge { [NotNull] get; [NotNull] set; }
     }
 }
 #endif
